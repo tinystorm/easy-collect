6ef082880922b3b76acda69c11482e5456a54e37
TinyStorm <1742451174@qq.com>
2020-04-01 21:39:44 +0800

    maven项目结构提交

---
+++ src/main/java/org/meng/easycollect/EasyCollectApplication.java
@@ -0,0 +1,16 @@
+package org.meng.easycollect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * easy collect 启动类
+ * @author Meng
+ * @date 2020/04/01
+ */
+@SpringBootApplication
+public class EasyCollectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EasyCollectApplication.class);
+    }
+}
